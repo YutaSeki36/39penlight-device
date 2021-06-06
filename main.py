c55ad80dd7ba2148ef2be4586a67a55f1164e297
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-06 17:38:59 +0900

    カラーコードからrgbを算出する処理を追加

--- main.py
+++ main.py
@@ -24,25 +24,32 @@ def lightUp(red=0,green=0,blue=0):
 def cleanup():
     pixels.fill((0, 0, 0))
 
+def hex_to_rgb(hex):
+    hex = hex.lstrip('#')
+    hlen = len(hex)
+    return tuple(int(hex[i:i + hlen // 3], 16) for i in range(0, hlen, hlen // 3))
+
 def on_connect(client, userdata, flags, respons_code):
     print('status {}'.format(respons_code))
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
     print(msg.payload.decode('utf-8'))
+    try:
+        rgb = hex_to_rgb(msg.payload.decode('utf-8'))
+        stripe(rgb[0], rgb[1], rgb[2])
 
 def main():
-    client = mqtt.Client()
-    client.username_pw_set("token:%s"%TOKEN)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(HOSTNAME, port=PORT, keepalive=60)
-    client.loop_forever()
-
     try:
-        stripe(0, 255, 0)
-        while True:
-            lightUp(0, 255, 0)
+        client = mqtt.Client()
+        client.username_pw_set("token:%s"%TOKEN)
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(HOSTNAME, port=PORT, keepalive=60)
+        client.loop_forever()
+        # stripe(0, 255, 0)
+        # while True:
+        #     lightUp(0, 255, 0)
 
     finally:
         cleanup()
